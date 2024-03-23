3663001be594de4843d33cd6df238da7618f19f2
andrewturcotte166 <turcotte.an@northeastern.edu>
2024-03-22 20:05:08 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import Hello from "./hello.js"
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from './Kanbas/Courses/routes.js';
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+const app = express()
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+Hello(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000)
---
+++ Kanbas/Database/index.js
@@ -0,0 +1,9 @@
+import courses from "./courses.js";
+import modules from "./modules.js";
+import assignments from "./assignments.js";
+import todos from "./todos.js";
+import calendar from "./calendar.js";
+import users from "./users.js";
+import grades from "./grades.js";
+import enrollments from "./enrollments.js";
+export default { courses, modules, assignments, todos, calendar, users, grades, enrollments };
