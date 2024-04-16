4c86c29e490c952367393c5b1caa0da7017676d4
andrewturcotte166 <turcotte.an@northeastern.edu>
2024-04-16 14:03:07 -0400

    multiple users

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import "dotenv/config";
+import session from "express-session";
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
 import Hello from "./hello.js"
@@ -11,8 +12,25 @@ const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(
-    cors()
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
 );
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
